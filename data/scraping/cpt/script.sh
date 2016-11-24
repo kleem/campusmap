ca33e2d549b028a610dcf955582ffc6da0556bd1
andreaderrico2 <andreaderrico2@gmail.com>
2016-11-24 14:59:24 +0100

    change resource url for cpt

--- data/scraping/cpt/script.sh
+++ data/scraping/cpt/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #wget http://dati.toscana.it/dataset/rt-oraritb/resource/70d34dbc-259e-4051-b51e-979ace4fcc9b -O - 2>/dev/null | grep -oP 'href="\Khttp:.+?"' | sed 's/"//' | grep gtfs | sed '2d' >url.txt
-wget http://dati.toscana.it/dataset/rt-oraritb/resource/e596c8fa-3c8b-4a2d-862e-26f52691a113 -O - 2>/dev/null | grep -oP 'href="\Khttp:.+?"' | sed 's/"//' | grep gtfs | sed '2d' >url.txt
+wget http://dati.toscana.it/dataset/rt-oraritb/resource/9dd1ed78-5854-4341-8fd2-0ecc8edb5016 -O - 2>/dev/null | grep -oP 'href="\Khttp:.+?"' | sed 's/"//' | grep gtfs | sed '2d' >url.txt
 URL="$(grep 'http' url.txt)"
 wget "${URL}"
 unzip cpt.gtfs*
